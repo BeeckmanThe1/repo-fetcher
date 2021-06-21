deab8065eeca5832078a1a04b4f50989260b7bfe
Thomas Opdebeeck <thomas.opdebeeck@realdolmen.com>
2021-06-21 20:07:03 +0200

    Ordering: step 1

--- hybrid/sections/repositoriesTableSection/RepositoriesTableSection.jsx
+++ hybrid/sections/repositoriesTableSection/RepositoriesTableSection.jsx
@@ -7,13 +7,50 @@ import websiteActions from '../../store/actions/website/website.action';
 
 const RepositoriesTableSection = () => {
 
-    //storageHelper.setItem(rankingStoreKey, rank)
-
     const [didMount, setDidMount] = useState(false);
+    const [ordering, setOrdering] = useState({rank: undefined});
+
     const repos = useSelector(state => state?.website?.repositories?.items) || [];
     const dispatch = useDispatch();
 
     useEffect(() => setDidMount(true), []);
+    const mapper = {
+        'asc': 'up',
+        'desc': 'down',
+        undefined: 'right'
+    }
+
+    const sortRepos = repos => {
+        const sortedRepos = repos.sort((repo1, repo2) => {
+            if (ordering?.rank === 'asc') {
+                return repo1.rank >= repo2.rank;
+            } else if (ordering?.rank === 'desc') {
+                return repo1?.rank <= repo2?.rank
+            } else return 0;
+        });
+
+        return sortedRepos;
+    };
+
+    const changeOrdering = type => {
+        setOrdering(prev => {
+            const prevRanking = prev?.[type];
+            let newOrdening;
+            switch (prevRanking) {
+                case 'asc':
+                    newOrdening = 'desc';
+                    break;
+                case 'desc':
+                    newOrdening = undefined;
+                    break;
+                case undefined:
+                    newOrdening = 'asc';
+                    break;
+            }
+            const newOrdering = {...prev, [type]: newOrdening};
+            return newOrdering;
+        });
+    }
 
     return <section className={classnames('rep-repositories-table-section', 'container')}>
         <table className="table">
@@ -22,14 +59,15 @@ const RepositoriesTableSection = () => {
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
                 <th scope="col">Author</th>
-                <th scope="col">⭐<i className="fas fa-caret-up"/></th>
+                <th scope="col">Ranking<i onClick={() => changeOrdering('rank')} className={classnames(`fas fa-caret-${mapper[ordering?.rank]}`)}/></th>
                 <th scope="col"></th>
             </tr>
             </thead>
             <tbody>
-            {repos.map((repo, index) => {
+            {sortRepos(repos).map((repo, index) => {
                 const rankingStoreKey = `${repo?.name}.ranking` || 0;
                 const storedRanking = didMount && storageHelper.getItem(rankingStoreKey);
+                //storedRanking && dispatch(websiteActions.rankRepo(repo?.name, storedRanking));
                 return <tr>
                     <th scope="row">{index + 1}</th>
                     <td>{repo.name}</td>

7906bb144a06b8e8a0e8061fec70a74e02171fc6
Thomas Opdebeeck <thomas.opdebeeck@realdolmen.com>
2021-06-21 22:49:31 +0200

    test shouldBeGold util

--- hybrid/partials/RankingStars/RankingStars.jsx
+++ hybrid/partials/RankingStars/RankingStars.jsx
@@ -1,23 +1,18 @@
 import React, {useState} from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import {shouldStarBeGold} from "./RankingStars.util";
 
 const RankingStars = ({numberOfStars, ranking, handleRanking}) => {
     const [hoveredStar, setHoveredStar] = useState();
     const [rankingState, setRankingState] = useState();
     const stars = new Array(numberOfStars).fill('');
-    const shouldStarBeGold = (rankingCurrentStar, ranking) => {
-        const isHovering = !!hoveredStar;
-        const hasRanking = !!ranking;
 
-        const shouldBeGold = isHovering ? rankingCurrentStar <= hoveredStar : hasRanking ? rankingCurrentStar <= ranking : false;
-        return shouldBeGold;
-    }
     return <div className={'rep-ranking-stars'}>
         {stars.map((star, index) => {
             const rankingCurrentStar = index + 1;
             return <i onClick={() => handleRanking(rankingCurrentStar)}
-                      className={classnames({'rep-golden-star': shouldStarBeGold(rankingCurrentStar, ranking)}, 'fas fa-star')}
+                      className={classnames({'rep-golden-star': shouldStarBeGold(rankingCurrentStar, ranking, hoveredStar)}, 'fas fa-star')}
                       onMouseOver={() => setHoveredStar(rankingCurrentStar)}
                       onMouseLeave={() => setHoveredStar()}/>
         })}
---
+++ hybrid/partials/RankingStars/RankingStars.util.js
@@ -0,0 +1,7 @@
+export const shouldStarBeGold = (currentStar, ranking, hoveredStar) => {
+    const isHovering = !!hoveredStar;
+    const hasRanking = !!ranking;
+
+    const shouldBeGold = isHovering ? currentStar <= hoveredStar : hasRanking ? currentStar <= ranking : false;
+    return shouldBeGold;
+}
